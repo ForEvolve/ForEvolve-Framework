5dd54511f079786d210bd94879bd47189688388c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-11-02 15:54:10 -0400

    Removed a comment

--- src/ForEvolve.AspNetCore/Services/IViewRenderer.cs
+++ src/ForEvolve.AspNetCore/Services/IViewRenderer.cs
@@ -1,10 +1,4 @@
-﻿//
-// Instead of converting my own .NET 4 version of this service, 
-// I found an article titled "ASP.NET Core Render View to String".
-// I may update this code at some point, until then, thanks to the author(s).
-// Link: https://ppolyzos.com/2016/09/09/asp-net-core-render-view-to-string/
-//
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace ForEvolve.AspNetCore.Services
 {
