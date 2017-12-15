d615287187057101ee724d61bb481bf1c383ef21
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-12-15 17:48:13 -0500

    Added BaseHttpTest

---
+++ src/ForEvolve.XUnit/HttpTests/BaseHttpTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+
+namespace ForEvolve.XUnit.HttpTests
+{
+    public abstract class BaseHttpTest<TStartup> : BaseHttpTest
+         where TStartup : class
+    {
+        public BaseHttpTest()
+            : base(WebHost.CreateDefaultBuilder().UseStartup<TStartup>())
+        {
+        }
+    }
+
+    public abstract class BaseHttpTest : IDisposable
+    {
+        protected TestServer Server { get; }
+        protected HttpClient Client { get; }
+
+        protected virtual Uri BaseAddress => new Uri("http://localhost");
+        protected virtual string Environment => "Development";
+
+        public BaseHttpTest()
+            : this(WebHost.CreateDefaultBuilder())
+        {
+        }
+
+        public BaseHttpTest(IWebHostBuilder webHostBuilder)
+        {
+            if (webHostBuilder == null) { throw new ArgumentNullException(nameof(webHostBuilder)); }
+
+            var builder = webHostBuilder
+                .UseEnvironment(Environment)
+                .ConfigureServices(ConfigureServices);
+
+            Server = new TestServer(builder);
+            Client = Server.CreateClient();
+            Client.BaseAddress = BaseAddress;
+        }
+
+        protected virtual void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        #region IDisposable Support
+
+        private bool _isDisposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    Client.Dispose();
+                    Server.Dispose();
+                }
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
---
+++ test/ForEvolve.XUnit.Tests/HttpTests/BaseHttpTestTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ForEvolve.XUnit.HttpTests
+{
+    public class BaseHttpTestTest : BaseHttpTest<Startup>
+    {
+        [Fact]
+        public async Task Should_setup_the_client_and_the_server()
+        {
+            // Arrange
+            var expectedStatusCode = Startup.DefaultStatusCode;
+
+            // Act
+            var result = await Client.GetAsync("/");
+
+            // Assert
+            Assert.Equal(expectedStatusCode, (int)result.StatusCode);
+        }
+
+        public class ConfigureServicesTest : BaseHttpTest<Startup>
+        {
+            private const int ExpectedStatusCode = StatusCodes.Status202Accepted;
+
+            [Fact]
+            public async Task Should_setup_the_client_and_the_server()
+            {
+                // Arrange
+                var expectedStatusCode = ExpectedStatusCode;
+
+                // Act
+                var result = await Client.GetAsync("/");
+
+                // Assert
+                Assert.Equal(expectedStatusCode, (int)result.StatusCode);
+            }
+
+            protected override void ConfigureServices(IServiceCollection services)
+            {
+                services.TryAddSingleton<IStatusCodeProvider>(new StatusCodeProvider(ExpectedStatusCode));
+            }
+        }
+
+    }
+
+    public class Startup
+    {
+        public const int DefaultStatusCode = StatusCodes.Status200OK;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.TryAddSingleton<IStatusCodeProvider>(new StatusCodeProvider(DefaultStatusCode));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(context =>
+            {
+                var statusCodeProvider = app.ApplicationServices
+                    .GetRequiredService<IStatusCodeProvider>();
+                context.Response.StatusCode = statusCodeProvider.StatusCode;
+                return context.Response.WriteAsync(context.Request.Path);
+            });
+        }
+    }
+
+    public interface IStatusCodeProvider
+    {
+        int StatusCode { get; }
+    }
+
+    public class StatusCodeProvider : IStatusCodeProvider
+    {
+        public StatusCodeProvider(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+        public int StatusCode { get; }
+    }
+
+}
