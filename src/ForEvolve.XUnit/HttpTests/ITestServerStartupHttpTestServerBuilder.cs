f23fd6445ff0c8762245756706f208208815b2c3
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-12-17 16:56:20 -0500

    Extracted HttpTestServer classes and interfaces to their own file.

---
+++ src/ForEvolve.XUnit/HttpTests/ITestServerStartupHttpTestServerBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace ForEvolve.XUnit.HttpTests
+{
+    public interface ITestServerStartupHttpTestServerBuilder : IHttpTestServerBuilder<TestServerStartup>
+    {
+        IHttpTestServer Arrange<TStatusCodeProvider>()
+            where TStatusCodeProvider : class, IStatusCodeProvider;
+
+        IHttpTestServer Arrange<TStatusCodeProvider, TResponseProvider>()
+            where TStatusCodeProvider : class, IStatusCodeProvider
+            where TResponseProvider : class, IResponseProvider;
+
+        IHttpTestServer Arrange(IStatusCodeProvider statusCodeProvider);
+        IHttpTestServer Arrange(IResponseProvider responseProvider);
+        IHttpTestServer Arrange(IStatusCodeProvider statusCodeProvider, IResponseProvider responseProvider);
+
+        IHttpTestServer Arrange<TStatusCodeProvider>(IResponseProvider responseProvider)
+            where TStatusCodeProvider : class, IStatusCodeProvider;
+        IHttpTestServer Arrange<TResponseProvider>(IStatusCodeProvider statusCodeProvider)
+            where TResponseProvider : class, IResponseProvider;
+    }
+}
---
+++ src/ForEvolve.XUnit/HttpTests/TestServerStartupHttpTestServerBuilder.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ForEvolve.XUnit.HttpTests
+{
+    public class TestServerStartupHttpTestServerBuilder : HttpTestServerBuilder<TestServerStartup>, ITestServerStartupHttpTestServerBuilder
+    {
+        public IHttpTestServer Arrange<TStatusCodeProvider>()
+            where TStatusCodeProvider : class, IStatusCodeProvider
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient<IStatusCodeProvider, TStatusCodeProvider>();
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange<TStatusCodeProvider, TResponseProvider>()
+            where TStatusCodeProvider : class, IStatusCodeProvider
+            where TResponseProvider : class, IResponseProvider
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient<IStatusCodeProvider, TStatusCodeProvider>();
+                    services.AddTransient<IResponseProvider, TResponseProvider>();
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange(IStatusCodeProvider statusCodeProvider)
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient(x => statusCodeProvider);
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange(IResponseProvider responseProvider)
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient(x => responseProvider);
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange(IStatusCodeProvider statusCodeProvider, IResponseProvider responseProvider)
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient(x => statusCodeProvider);
+                    services.AddTransient(x => responseProvider);
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange<TStatusCodeProvider>(IResponseProvider responseProvider)
+            where TStatusCodeProvider : class, IStatusCodeProvider
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient<IStatusCodeProvider, TStatusCodeProvider>();
+                    services.AddTransient(x => responseProvider);
+                });
+            });
+        }
+
+        public IHttpTestServer Arrange<TResponseProvider>(IStatusCodeProvider statusCodeProvider)
+            where TResponseProvider : class, IResponseProvider
+        {
+            return Arrange(hostBuilder =>
+            {
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddTransient(x => statusCodeProvider);
+                    services.AddTransient<IResponseProvider, TResponseProvider>();
+                });
+            });
+        }
+    }
+}
