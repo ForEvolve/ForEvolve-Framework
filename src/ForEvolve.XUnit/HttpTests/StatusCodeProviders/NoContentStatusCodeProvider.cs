bdcf30c1b6d137b8046797bc5f681efc9173d5fc
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-12-17 20:42:53 -0500

    Added NoContentStatusCodeProvider

---
+++ src/ForEvolve.XUnit/HttpTests/StatusCodeProviders/NoContentStatusCodeProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ForEvolve.XUnit.HttpTests
+{
+    public sealed class NoContentStatusCodeProvider : StatusCodeProvider
+    {
+        public const int ExpectedStatusCode = StatusCodes.Status204NoContent;
+
+        public NoContentStatusCodeProvider() : base(ExpectedStatusCode) { }
+    }
+
+}
