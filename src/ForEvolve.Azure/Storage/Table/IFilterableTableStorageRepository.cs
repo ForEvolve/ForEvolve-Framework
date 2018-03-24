e7204427220e0bc8459029679d70c7ff5ec2c627
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-03-24 11:36:12 -0400

    Added FilterableTableStorageRepository

---
+++ src/ForEvolve.Azure/Storage/Table/IFilterableTableStorageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace ForEvolve.Azure.Storage.Table
+{
+    public interface IFilterableTableStorageRepository<TModel>
+         where TModel : ITableEntity, new()
+    {
+        Task<IEnumerable<TModel>> ReadAsync(Func<TableQuery<TModel>, TableQuery<TModel>> filter);
+    }
+}
--- src/ForEvolve.Azure/Storage/Table/TableStorageRepository.cs
+++ src/ForEvolve.Azure/Storage/Table/TableStorageRepository.cs
@@ -147,4 +147,26 @@ namespace ForEvolve.Azure.Storage.Table
             }
         }
     }
+
+    public class FilterableTableStorageRepository<TModel> : TableStorageRepository<TModel>, IFilterableTableStorageRepository<TModel>
+        where TModel : class, ITableEntity, new()
+    {
+        public FilterableTableStorageRepository(ITableStorageSettings storageSettings)
+            : base(storageSettings)
+        {
+        }
+
+        public FilterableTableStorageRepository(CloudTable table)
+            : base(table)
+        {
+        }
+
+        public async Task<IEnumerable<TModel>> ReadAsync(Func<TableQuery<TModel>, TableQuery<TModel>> filter)
+        {
+            if (filter == null) { throw new ArgumentNullException(nameof(filter)); }
+
+            var query = filter(new TableQuery<TModel>());
+            return await QueryTableAsync(query);
+        }
+    }
 }
