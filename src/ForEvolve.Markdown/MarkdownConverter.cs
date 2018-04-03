59779371cdc5001c47d86dbf7954c54df7e559a3
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-04-03 16:34:01 -0400

    Initial markdown code

---
+++ src/ForEvolve.Markdown/MarkdownConverter.cs
@@ -0,0 +1,29 @@
+﻿using Markdig;
+using System;
+
+namespace ForEvolve.Markdown
+{
+    public interface IMarkdownConverter
+    {
+        string ConvertToHtml(string markdown);
+    }
+
+    public class MarkdownConverter : IMarkdownConverter
+    {
+        private readonly MarkdownPipeline _pipeline;
+
+        public MarkdownConverter(MarkdownPipeline pipeline)
+        {
+            _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
+        }
+
+        public string ConvertToHtml(string markdown)
+        {
+            if (string.IsNullOrEmpty(markdown))
+            {
+                return markdown;
+            }
+            return Markdig.Markdown.ToHtml(markdown, _pipeline);
+        }
+    }
+}
---
+++ src/ForEvolve.Markdown/MarkdownStartupExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ForEvolve.Markdown;
+using Markdig;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MarkdownStartupExtensions
+    {
+        public static IServiceCollection AddMarkdown(this IServiceCollection services, Action<MarkdownPipelineBuilder> configure = null)
+        {
+            services.TryAddSingleton<IMarkdownConverter, MarkdownConverter>();
+            services.AddSingleton(provider => {
+                var pipeline = new MarkdownPipelineBuilder();
+                configure?.Invoke(pipeline);
+                return pipeline.Build();
+            });
+            return services;
+        }
+    }
+}
