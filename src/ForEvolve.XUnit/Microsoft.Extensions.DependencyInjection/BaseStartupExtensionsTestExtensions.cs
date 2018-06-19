98e8be8516e4bf80e46e64a61bdb13df7396f879
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-06-19 16:38:06 -0400

    Replaced BaseStartupExtensionsTest by IServiceCollection extensions methods.
    Assert methods are fluent

--- src/ForEvolve.XUnit/Microsoft.Extensions.DependencyInjection/BaseStartupExtensionsTestExtensions.cs
+++ src/ForEvolve.XUnit/Microsoft.Extensions.DependencyInjection/BaseStartupExtensionsTestExtensions.cs
@@ -1,13 +1,15 @@
 ﻿using Moq;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class BaseStartupExtensionsTestExtensions
     {
-        public static IServiceCollection AddSingletonMock<TService>(this IServiceCollection services)
+        public static IServiceCollection AddSingletonMock<TService>(this IServiceCollection services, Action<Mock<TService>> setup = null)
             where TService : class
         {
             var serviceMock = new Mock<TService>();
+            setup?.Invoke(serviceMock);
             services.AddSingleton(serviceMock.Object);
             return services;
         }
