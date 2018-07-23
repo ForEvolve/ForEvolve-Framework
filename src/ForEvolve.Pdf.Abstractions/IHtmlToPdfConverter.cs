8485fcf052b045d2f95cbba019aa704348dd216d
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-07-23 17:37:13 -0400

    Add IHtmlToPdfConverter

---
+++ src/ForEvolve.Pdf.Abstractions/IHtmlToPdfConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForEvolve.Pdf.Abstractions
+{
+    /// <summary>
+    /// Convert HTML to PDF.
+    /// </summary>
+    public interface IHtmlToPdfConverter
+    {
+        /// <summary>
+        /// Render the specified HTML code to a PDF.
+        /// Save that PDF to the specified directory.
+        /// </summary>
+        /// <param name="html">The HTML to convert to PDF.</param>
+        /// <param name="outputFolder">The directory to save the PDF to.</param>
+        /// <returns>The full file path of the generated PDF.</returns>
+        string Convert(string html, string outputFolder);
+    }
+}
