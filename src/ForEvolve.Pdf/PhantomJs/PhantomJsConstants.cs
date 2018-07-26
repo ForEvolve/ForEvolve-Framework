7eddbe757293006fcec47cf9b04982b757790e2e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-07-26 16:10:24 -0400

    Rename ForEvolve.Pdf.PhantomJs.Dependencies to ForEvolve.PhantomJs.Dependencies

--- src/ForEvolve.Pdf/PhantomJs/PhantomJsConstants.cs
+++ src/ForEvolve.Pdf/PhantomJs/PhantomJsConstants.cs
@@ -5,6 +5,6 @@
         public const string OSNotSupported = "Your operating system is not supported or could not be discovered.";
         public const string OutputDirectoryDoesNotExist = "The specified output directory does not exist.";
         public const string DirectoryDoesNotExist = "The specified directory does not exist.";
-        public const string PhantomJSExeNotFound = "The PhantomJS executable file was not found. Please make sure you installed the `ForEvolve.Pdf.PhantomJs.Dependencies` NuGet package.";
+        public const string PhantomJSExeNotFound = "The PhantomJS executable file was not found. Please make sure you installed the `ForEvolve.PhantomJs.Dependencies` NuGet package.";
     }
 }
