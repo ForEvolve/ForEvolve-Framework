5eac1ae9603845d1de62fb22f1d38c13f6b19eb3
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-07 16:25:27 -0500

    Remove Obsolete test code

--- test/ForEvolve.AspNetCore.Tests/ErrorFactory/Implementations/ObsoleteMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ForEvolve
-{
-    public class ObsoleteMessage
-    {
-        public const string Xunit = "This was deleted with ForEvolve.XUnit and an equivalent should be created into ForEvolve.Testing.";
-    }
-}
