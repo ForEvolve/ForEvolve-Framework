8f86b7dff70dfa819e23d3938fc707888b074ef1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-11 15:08:17 -0400

    Add a test for OperationResultStartupExtensions

--- src/ForEvolve.OperationResults.AspNetCore/Standardizer/OperationResultStartupExtensions.cs
+++ src/ForEvolve.OperationResults.AspNetCore/Standardizer/OperationResultStartupExtensions.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddForEvolveOperationResultStandardizer(this IServiceCollection services)
         {
             services
+                .AddLogging()
                 .AddSingleton<IPropertyNameFormatter, DefaultPropertyNameFormatter>()
                 .AddSingleton<IPropertyValueFormatter, DefaultPropertyValueFormatter>()
                 .AddSingleton<IOperationResultStandardizer, DefaultOperationResultStandardizer>()
---
+++ test/ForEvolve.OperationResults.AspNetCore.Tests/Standardizer/OperationResultStartupExtensionsTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ForEvolve.OperationResults.Standardizer
+{
+    [Collection(OperationResultStartupExtensionsServerCollection.Name)]
+    public class OperationResultStartupExtensionsTest
+    {
+        private OperationResultStartupExtensionsServerFixture _server;
+        public OperationResultStartupExtensionsTest(OperationResultStartupExtensionsServerFixture server)
+        {
+            _server = server ?? throw new ArgumentNullException(nameof(server));
+        }
+
+        [Fact]
+        public async Task Should_standardize_OkObjectResult()
+        {
+            // Arrange
+            var expectedBody = "{\"" + DefaultOperationResultStandardizerOptions.DefaultOperationName + "\":";
+            expectedBody += "{\"messages\":[],\"succeeded\":true},";
+            expectedBody += "\"someProp\":\"Oh Yeah!\",\"someOtherProp\":true}";
+
+            // Act
+            var result = await _server.Client.GetAsync("/OperationResultStartupExtensionsTestController/OkObjectResult");
+
+            // Assert
+            result.EnsureSuccessStatusCode();
+            var body = await result.Content.ReadAsStringAsync();
+            Assert.Equal(expectedBody, body);
+        }
+
+    }
+
+    public class OperationResultStartupExtensionsServerFixture
+    {
+        public TestServer Server { get; }
+        public HttpClient Client { get; }
+
+        public OperationResultStartupExtensionsServerFixture()
+        {
+            //Action<IServiceCollection> configureServices
+            //Action<IApplicationBuilder> configureApp
+            var hostBuilder = WebHost.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddForEvolveOperationResultStandardizer();
+                    services.AddMvc();
+                })
+                .Configure(app =>
+                {
+                    app.UseMvc();
+                })
+                ;
+            Server = new TestServer(hostBuilder);
+            Client = Server.CreateClient();
+        }
+    }
+
+    [CollectionDefinition(Name)]
+    public class OperationResultStartupExtensionsServerCollection : ICollectionFixture<OperationResultStartupExtensionsServerFixture>
+    {
+        public const string Name = "OperationResultStartupExtensions Server";
+    }
+
+    [Route("OperationResultStartupExtensionsTestController")]
+    public class OperationResultStartupExtensionsTestController : ControllerBase
+    {
+        [HttpGet("OkObjectResult")]
+        public IActionResult OkObjectResult()
+        {
+            var result = OperationResult.Success(new { SomeProp = "Oh Yeah!", SomeOtherProp = true });
+            return Ok(result);
+        }
+    }
+}
