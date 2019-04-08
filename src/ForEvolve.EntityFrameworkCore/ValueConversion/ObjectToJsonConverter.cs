494551b2461c8175903501a934493492495356df
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-04-08 15:05:34 -0400

    Cleanup to trigger rebuild

--- src/ForEvolve.EntityFrameworkCore/ValueConversion/ObjectToJsonConverter.cs
+++ src/ForEvolve.EntityFrameworkCore/ValueConversion/ObjectToJsonConverter.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ForEvolve.EntityFrameworkCore.ValueConversion
 {
