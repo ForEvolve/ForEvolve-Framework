8288db5d67be17a78af81e1aa6910b18146b64d2
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-04-10 17:18:26 -0400

    Add NotImplementedExceptionFilterAttribute

---
+++ src/ForEvolve.AspNetCore/ExceptionFilters/NotImplementedExceptionFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEvolve.AspNetCore.ExceptionFilters
+{
+    /// <summary>
+    /// Represent a filter that runs asynchronously after an action has thrown an System.Exception.
+    /// If the exception is an <see cref="NotImplementedException"/>, the filter convert the status code from 500 to 501.
+    /// Implements the <see cref="ExceptionFilterAttribute" />
+    /// </summary>
+    /// <seealso cref="ExceptionFilterAttribute" />
+    public class NotImplementedExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>
+        /// Called when an exception is thrown in the MVC pipeline.
+        /// </summary>
+        /// <param name="context">The exception context.</param>
+        /// <inheritdoc />
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is NotImplementedException)
+            {
+                context.HttpContext.Response.StatusCode = 501;
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
