ee1f6a402b69221139e1768c9b6e8e11595c3abc
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-06-11 12:21:52 -0400

    Add ForEvolveMediatRExtensions

---
+++ src/ForEvolve.OperationResults.AspNetCore/MediatR/ForEvolveMediatRExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Scrutor;
+
+namespace ForEvolve.OperationResults.MediatR
+{
+    /// <summary>
+    /// Contains extension methods to help wire up MediatR.
+    /// </summary>
+    public static class ForEvolveMediatRExtensions
+    {
+        /// <summary>
+        /// Registers all <see cref="ValidationBehavior{TRequest, TResponse}"/> 
+        /// that uses <see cref="IValidator{TRequest}"/> 
+        /// as MediatR <see cref="IPipelineBehavior{TRequest, TResponse}"/>.
+        /// 
+        /// Automatic command and query validation should work as long there is a <see cref="IValidator{TRequest}"/> for the handler.
+        /// </summary>
+        /// <param name="selector">The selector used to scan for classes.</param>
+        /// <returns>The selector.</returns>
+        public static IImplementationTypeSelector AddValidationBehaviors(this IImplementationTypeSelector selector)
+        {
+            return selector.AddClasses(classes => classes.AssignableTo(typeof(ValidationBehavior<,>)))
+                .As(typeof(IPipelineBehavior<,>))
+                .WithTransientLifetime();
+        }
+    }
+}
