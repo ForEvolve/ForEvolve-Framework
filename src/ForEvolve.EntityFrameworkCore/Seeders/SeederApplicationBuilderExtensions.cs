1c5e5985d2499bd532bf6e289873de06bc4538eb
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-05-11 14:16:23 -0400

    Add IApplicationBuilder.Seed() extension method

---
+++ src/ForEvolve.EntityFrameworkCore/Seeders/SeederApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEvolve.EntityFrameworkCore.Seeders
+{
+    public static class SeederApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Find the <see cref="ISeederManager{TDbContext}"/> and call its Seed method,
+        /// executing all the <see cref="ISeeder{TDbContext}"/> that were registered.
+        /// </summary>
+        /// <typeparam name="TDbContext"></typeparam>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder Seed<TDbContext>(this IApplicationBuilder app)
+            where TDbContext : DbContext
+        {
+            var factory = app.ApplicationServices.GetService<ISeederManagerFactory>();
+            var manager = factory.Create<TDbContext>();
+            manager.Seed();
+            return app;
+        }
+    }
+}
